5decc3e638b61393c4197d890f33875d4b96c654
Amber <anborong@outlook.com>
2020-12-12 15:53:29 +0800

    无头浏览器

--- src/config.ts
+++ src/config.ts
@@ -8,7 +8,7 @@ export const AppConfig = {
     prefix: isProduct ? "/data/" : "./test/",               // 文件路径
     minSizeMb: 0.1,             // 文件最小阈值
     startUrl: "https://www.nvshens.org/",
-    headless: false,
+    headless: isProduct ? true :　false,
     executablePath: isProduct ? "/usr/bin/google-chrome-stable" : undefined,
     redis: isProduct ? "127.0.0.1" : "192.168.0.166"
 }
