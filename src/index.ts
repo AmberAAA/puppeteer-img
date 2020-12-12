aaa4c28e9c34c9f127a2a083ed51cf852a559d63
Amber <anborong@outlook.com>
2020-12-12 11:56:34 +0800

    init

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,12 @@
 import puppeteer, { Response } from 'puppeteer';
 import { launchConfig, AppConfig } from './config';
 import fs from  "fs";
-let i = 0;
 
 (async function () {
     const brower = await puppeteer.launch(launchConfig);
     const page = await brower.newPage();
     page.on("response",async (e, ...arr) => {
         await handleResponse(e);
-        i++;
-        if (i >= 5) {
-            console.log("END........");
-            process.exit(0);
-        }
     })
     page.goto(AppConfig.startUrl);
 })()
